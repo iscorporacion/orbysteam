8ba3eeb93d7502863ede7ba0ad0575cc36adf578
taylor mosquera castro <thaylors@gmail.com>
2020-03-27 15:18:52 -0500

    agrega soporte para librerias y archivos publicos

--- src/orbysteam.js
+++ src/orbysteam.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 const morgan = require("morgan");
+const favicon = require('serve-favicon');
+
 const teamRouter = require('./routes/team');
+
 const app = express();
 
 app.set('port', process.env.PORT_ORBYSTEAM || 3000)
@@ -9,6 +12,9 @@ app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'..','node_modules')));
+app.use(favicon(path.join(__dirname,'public','img','favicon','favicon.ico')));
 app.use('/',teamRouter);
 
 
--- src/routes/team.js
+++ src/routes/team.js
@@ -12,7 +12,7 @@ router.get("/team",(req,res) => {
 
 
 router.get("/registroUsuario",(req,res) => {
-    res.render("team");
+    res.render("registroUsuario");
 });
 
 module.exports = router;
